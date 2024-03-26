757d43ab14b3211b5bd496acafef93580041bf5e
thayna.ribeiro13 <ribeirothayna640@gmail.com>
2024-03-25 22:33:00 -0300

    first commit

---
+++ AvaliacaoDiceRoller/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AvaliacaoDiceRoller
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AvaliacaoDiceRoller/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml.Serialization;
+
+namespace AvaliacaoDiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                RollerBtn.Text = picker.ItemsSource[selectedIndex].ToString();
+            }
+        }
+
+        private void RollerBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private class RandomDiceRoller : Random
+        {
+            Random rnd = new Random();
+        }
+    }
+
+}
